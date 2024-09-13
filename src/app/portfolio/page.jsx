52551904bddaf334f581b14d2e35a0c3cbdf0a7a
latikaguptak <08guptal@gmail.com>
2024-09-13 20:28:22 +0530

    portfolio page added

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import style from'./page.module.css'
+
+import Link from 'next/link'
+import styles from'./page.module.css'
 const Portfolio = () => {
   return (
-    <div className={style.container} >Portfolio</div>
+    <div className={styles.container}>
+      <h1 className={styles.sTitle}>Choose from gallery</h1>
+      <div className={styles.items}>
+        <Link href='/portfolio/illustration' className={styles.item}>
+          <spam className={styles.title}>Illustrations</spam>
+        </Link>
+        <Link href='/portfolio/website' className={styles.item}>
+          <spam className={styles.title}>Website</spam>
+        </Link>
+        <Link href='/portfolio/applictaions' className={styles.item}>
+          <spam className={styles.title}>Applictaions</spam>
+        </Link>
+      </div>
+
+    </div>
   )
 }
 
