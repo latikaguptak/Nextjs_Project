6c89b53f02deb72ca383a1d01790be1a2c77574f
latikaguptak <08guptal@gmail.com>
2024-09-13 20:18:20 +0530

    about page created

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
-import style from'./page.module.css'
+import styles from'./page.module.css'
+import Image from 'next/image'
+import Button from '@/components/button/Button'
 const About = () => {
   return (
-    <div className={style.container}>About</div>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/About.png" 
+        fill={true}
+        alt=''
+        className={styles.img}/>
+
+        <div className={styles.imgText} >
+          <h1> Digital Storytellers</h1>
+          <h2>handcrafting award winning digital experience</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+         <div className={styles.item}>
+          <h1 className={ styles.title}>Who are we</h1>
+          <p className={styles.desc}>
+            lorem ipsum dolor sit amet, consectetur adip occurence velit sed diam non pro id el metus et justo vit euismod. Except   id liber tempor null a ante dict    sit aliquet consequ velit sed diam non pro id el metus et just o tempor null a ante dict sit aliquet consequ velit sed diam non pro id el metus et just o tempor null a ante dict sit aliquet consequ velit sed diam non pro.<br/>
+            Lorem ipsum dolor sit amet, consectetur adip occurence velit sed diam non pro id el metus et justo vit euismod. Except   id liber tempor null a ante dict    sit aliquet consequ velit sed diam non pro id el metus et just o tempor null a ante dict sit aliquet consequ velit sed diam non pro id el metus et just o tempor null a ante dict sit aliquet consequ velit sed diam non pro.
+          </p>
+         </div>
+         <div className={styles.item}>
+            <h1 className={ styles.title}>What we Do?</h1>
+            <p className={styles.desc}>
+            lorem ipsum dolor sit amet, consectetur adip occurence velit sed diam non pro id el metus et justo vit euismod.
+            <br/>
+            <br/>
+            -illution Science<br/>
+            <br/>
+            -fast and handling site<br/>
+            <br></br>
+            -learnings
+            </p>
+          <Button url='contact' text='Contact'/>
+         </div>
+        
+
+      </div>
+    </div>
   )
 }
 
