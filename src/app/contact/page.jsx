a680663aa85580009c2ff080352a141f232f10b2
latikaguptak <08guptal@gmail.com>
2024-09-13 20:20:07 +0530

    contact page added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import style from'./page.module.css'
+import styles from'./page.module.css'
+import Image from 'next/image'
+import Button from '@/components/button/Button'
 
 const Contact = () => {
   return (
-    <div className = {style.container}>Contact</div>
+    <div className = {styles.container}>
+      <h1 className={styles.title}>Let's Keep in touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+          src="/Contact.gif"
+          alt="Contact"
+          fill={true}
+          
+          className={styles.image} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" name="name" placeholder="Name"  className={styles.input}/>
+          <input type="email" name="email" placeholder="Email"  className={styles.input}/>
+          <textarea name="message" placeholder="Message" cols='30' rows='10' className={styles.textArea}></textarea>
+          <Button url='#' text="Send"/>
+        </form>
+      </div>
+    </div>
   )
 }
 
