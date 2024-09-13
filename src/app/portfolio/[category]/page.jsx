56489e68a007785570af718aac1e035b0529644a
latikaguptak <08guptal@gmail.com>
2024-09-13 20:31:05 +0530

    all pages and componenets updated

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import style from'./page.module.css'
-const Category = () => {
+import styles from'./page.module.css'
+const Category = ({params}) => {
+  console.log(params.category) // { category: 'web-development' }
   return (
-    <div className={style.container}>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        {(params.category.toUpperCase(1))}
+        </h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+
+        </div>
+        </div>
+       
+  
+    </div>
   )
 }
 
--- src/components/Hero/Hero.jsx
+++
@@ -1,9 +0,0 @@
-import Girl from '@'
-
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
-
-export default Hero
