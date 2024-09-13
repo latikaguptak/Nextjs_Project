02b49bcb3f70c5fb9cbc8f8e2bda727e96005de7
latikaguptak <08guptal@gmail.com>
2024-09-13 20:22:43 +0530

    layout page updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar/Navbar";
+import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
