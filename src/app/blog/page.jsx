5b3c07bcb489d12c41651a99f449d1392b555315
latikaguptak <08guptal@gmail.com>
2024-09-14 20:45:13 +0530

    added the blogs inside the blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
-import style from'./page.module.css'
+import styles from'./page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 const Blog = () => {
   return (
-    <div className={style.container}> Blog</div>
+    <div className={styles.maincontainer}> 
+    <Link href ='/blog/testId' className={styles.Container}>
+    <div className={styles.imgContainer}>
+      <Image 
+      src="/Blog.png"
+      alt=''
+      width={500}
+      height={300}
+      className={styles.image}
+      /
+      >
+
+    </div>
+    <div className={styles.content}>
+      <h1 className={styles.heading}>text</h1>
+      <p className={styles.desc}>Desc</p>
+    </div>
+  </Link>
+  <Link href ='/blog/testId' className={styles.Container}>
+
+    <div className={styles.imgContainer}>
+      <Image 
+      src="/Blog.png"
+      alt=''
+      width={500}
+      height={300}
+      className={styles.image}
+      /
+      >
+
+    </div>
+    <div className={styles.content}>
+      <h1 className={styles.heading}>text</h1>
+      <p className={styles.desc}>Desc</p>
+    </div>
+    </Link>
+    <Link href ='/blog/testId' className={styles.Container}>
+
+    <div className={styles.imgContainer}>
+      <Image 
+      src="/Blog.png"
+      alt=''
+      width={500}
+      height={300}
+      className={styles.image}
+      /
+      >
+
+    </div>
+    <div className={styles.content}>
+      <h1 className={styles.heading}>text</h1>
+      <p className={styles.desc}>Desc</p>
+    </div>
+    </Link>
+    </div>
+
   )
 }
 
