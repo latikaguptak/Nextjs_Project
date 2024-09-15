bfa3db5c76fd91be7460b389959911ac61d10a73
latikaguptak <08guptal@gmail.com>
2024-09-15 13:40:54 +0530

    Bug Fixed 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar/Navbar";
-import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./page.module.css";
-import Button from "@/components/Button/Button";
+import Button from "@/components/button/button";
 import Image from "next/image";
 const Category = ({ params }) => {
   console.log(params.category); // { category: 'web-development' }
