c8ca78d8e8bc2d976b72e2ec0d0295e70d856d1c
latikaguptak <08guptal@gmail.com>
2024-09-17 23:23:17 +0530

    Dark and light mode toggle created

--- components/DarkModeToggle/DarkModeToggle.jsx
+++ components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+'use client'
+
+import { useContext, useState } from "react";
 import styles from "./DarkModeToggle.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { TheamContext } from "../../context/TheamContext";
 
 const DarkModeToggle = () => {
 //   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -10,13 +13,14 @@ const DarkModeToggle = () => {
 //     setIsDarkMode(!isDarkMode);
 //   };
 
-    const mode= 'dark'
+const {toggle, mode} = useContext(TheamContext);
+
   return (
     <div
-      className={styles.container}>
+      className={styles.container} onClick={toggle}>
       <FontAwesomeIcon icon={faMoon} className={styles.icon} />
       <FontAwesomeIcon icon={faSun} className={styles.icon} />
-      <div className={styles.ball}  style= {mode==='light' ? {left:"2px"}:{right:"2px"}}/>
+      <div className={styles.ball}  style= {mode==='light' ? {left:"2px"}:{right:"2px"}} />
     </div>
   );
 };
