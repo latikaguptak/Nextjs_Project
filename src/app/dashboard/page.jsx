b3acb8a732148ffbea90e42e3fca2279d0feaa10
latikaguptak <08guptal@gmail.com>
2024-10-16 10:54:49 +0530

    update the application

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,66 +1,66 @@
-'use client'
-import { useEffect, useState } from 'react'
-import style from'./page.module.css'
-import useSWR from 'swr'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation';
+// 'use client'
+// import { useEffect, useState } from 'react'
+// import style from'./page.module.css'
+// import useSWR from 'swr'
+// import { useSession } from 'next-auth/react'
+// import { useRouter } from 'next/navigation';
 
-const Dashboard = () => {
-  const [isData,setIsData] = useState([]);
-  const [err,setErr] = useState(false);
-  const [isLoading,setIsLoading] = useState(false);
+// const Dashboard = () => {
+//   const [isData,setIsData] = useState([]);
+//   const [err,setErr] = useState(false);
+//   const [isLoading,setIsLoading] = useState(false);
 
-  // useEffect(() =>{
-  //   const getdata = async() => {
-  //     setIsLoading(true);
-  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-  //       cache: "no-store"
-  //     })
+//   // useEffect(() =>{
+//   //   const getdata = async() => {
+//   //     setIsLoading(true);
+//   //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//   //       cache: "no-store"
+//   //     })
 
-  //     if(!res.ok){
-  //       setErr(true);
-  //       setIsLoading(false);
-  //       return;
-  //     }
-  //     const data = await res.json();
-  //     setIsData(data);
-  //     setIsLoading(false);
+//   //     if(!res.ok){
+//   //       setErr(true);
+//   //       setIsLoading(false);
+//   //       return;
+//   //     }
+//   //     const data = await res.json();
+//   //     setIsData(data);
+//   //     setIsLoading(false);
 
-  //   };
+//   //   };
     
-  //   getdata()
-  // }, []);
+//   //   getdata()
+//   // }, []);
 
 
-const session = useSession();
-const router = useRouter()
+// const session = useSession();
+// const router = useRouter()
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const { data, error, loading } = useSWR(
-  `/api/posts?username=${session.data.user.name}`,
-  fetcher
-)
-console.log(
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// const { data, error, loading } = useSWR(
+//   `/api/posts?username=${session.data.user.name}`,
+//   fetcher
+// )
+// console.log(
  
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-  console.log(data);
-console.log(session);
-if (session.status === 'loading'){
-  return <p>Loading</p>
-}
-if (session.status === 'unauthenticated'){
-  router?.push('/dashboard/login');
-}
+//   if (error) return <div>Failed to load</div>
+//   if (!data) return <div>Loading...</div>
+//   console.log(data);
+// console.log(session);
+// if (session.status === 'loading'){
+//   return <p>Loading</p>
+// }
+// if (session.status === 'unauthenticated'){
+//   router?.push('/dashboard/login');
+// }
 
-  if (session.status === 'authenticated') {
-  return (
+//   if (session.status === 'authenticated') {
+//   return (
 
-    <div className={style.container}>Dashboard</div>
-  )
-}
-}
+//     <div className={style.container}>Dashboard</div>
+//   )
+// }
+// }
 
-export default Dashboard
+// export default Dashboard
 
 
